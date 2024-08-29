32b224314d034fb46b8ce9083073ad86de613d44
Addiel Abella <aabella2@huskers.unl.edu>
2024-08-29 15:34:42 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Addiel Abella
+ *         aabella2@huskers.unl.edu
+ * Date: 2024/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Addiel Abella!\n");
+
+  printf("My major is Computer Science\n");
+
+  return 0;
+}
